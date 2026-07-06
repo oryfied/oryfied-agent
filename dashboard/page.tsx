38fd7fd7f0f7f913bd7c6bf74ea15ef4bca262e6
oryfied <enralyakubi@gmail.com>
2026-07-06 10:27:15 +0700

    Create page.tsx

---
+++ dashboard/page.tsx
@@ -0,0 +1,20 @@
+import ProjectCard from "@/components/ProjectCard";
+import DeploymentCard from "@/components/DeploymentCard";
+
+export default function Dashboard() {
+
+  return (
+
+    <main>
+
+      <h1>Oryfied Engineering Dashboard</h1>
+
+      <ProjectCard />
+
+      <DeploymentCard />
+
+    </main>
+
+  );
+
+}
